9bbd8d5dbc64cffb1616ab24d4ef90c95e4a83cb
MKMKsGit <ratchitta.panyangam@gmail.com>
2022-06-06 13:38:09 +0700

    change state

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 // url for api
 // https://us-central1-skooldio-courses.cloudfunctions.net/react_01/random
 
+class Cell extends Component {
+  state = {
+    number: 0,
+  };
+  render() {
+    return (
+      <div
+        onClick={(e) => {
+          this.setState({
+            number: (this.state.number + 1) % 5
+          });
+        }}
+        className="cell"
+      >
+        {this.state.number != 0 && this.state.number}
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <div className="App">
         <div className="board">
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
         </div>
       </div>
     </div>
